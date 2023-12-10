ed8c5c2eb9806a0a0d6ffefea9a1e6fee1b07711
Marek Meyer <marek_meyer@outlook.com>
2023-12-10 14:30:35 +0100

    day 10 part 1

---
+++ src/day10.test.ts
@@ -0,0 +1,71 @@
+import { describe, expect, it } from "bun:test";
+import { getInputForDay } from "./getInput";
+import {
+  TyleMap,
+  countStepsThroughTheLoop,
+  getNewPositionForConnection,
+  parseInputToMap,
+} from "./day10";
+
+const testInput1 = `.....
+.S-7.
+.|.|.
+.L-J.
+.....`;
+
+const testInput2 = `..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...`;
+
+const input = await getInputForDay(10);
+
+describe("Part 1", () => {
+  it.each([
+    { input: testInput1, steps: 4 },
+    { input: testInput2, steps: 8 },
+    { input: input, steps: 4 },
+  ])(
+    "calculats the correct number of steps through the pipes to the farthest point",
+    ({ input, steps }) => {
+      expect(countStepsThroughTheLoop(parseInputToMap(input))).toBe(steps);
+    }
+  );
+
+  it.each([
+    {
+      input: testInput1,
+      expectedMap: [
+        [".", ".", ".", ".", "."],
+        [".", "S", "-", "7", "."],
+        [".", "|", ".", "|", "."],
+        [".", "L", "-", "J", "."],
+        [".", ".", ".", ".", "."],
+      ],
+    },
+    {
+      input: testInput2,
+      expectedMap: [
+        [".", ".", "F", "7", "."],
+        [".", "F", "J", "|", "."],
+        ["S", "J", ".", "L", "7"],
+        ["|", "F", "-", "-", "J"],
+        ["L", "J", ".", ".", "."],
+      ],
+    },
+  ])(
+    "calculats the correct number of steps through the pipes to the farthest point",
+    ({ input, expectedMap }) => {
+      expect(parseInputToMap(input)).toEqual(expectedMap as TyleMap);
+    }
+  );
+
+  describe("getNewPositionForConnection", () => {
+    it("creates a new position correctly", () => {
+      expect(
+        getNewPositionForConnection("east", { line: 1, index: 1 })
+      ).toEqual({ line: 1, index: 2 });
+    });
+  });
+});
---
+++ src/day10.ts
@@ -0,0 +1,136 @@
+type TyleSymbol = "|" | "-" | "L" | "J" | "7" | "F" | "." | "S";
+
+type TyleConnections = Record<TyleSymbol, [Connection, Connection] | undefined>;
+
+type Connection = "north" | "east" | "south" | "west";
+const possibleConnections: Connection[] = ["north", "east", "south", "west"];
+
+type PositionInMap = { line: number; index: number };
+
+export type TyleMap = TyleSymbol[][];
+
+const tyleConnections: TyleConnections = {
+  "|": ["north", "south"],
+  "-": ["west", "east"],
+  L: ["north", "east"],
+  J: ["north", "west"],
+  "7": ["south", "west"],
+  F: ["south", "east"],
+  ".": undefined,
+  S: undefined,
+};
+
+export const parseInputToMap = (input: string): TyleMap => {
+  return input.split("\n").map((line) => line.split("") as TyleSymbol[]);
+};
+
+export const countStepsThroughTheLoop = (tyleMap: TyleMap): number => {
+  const startLine = tyleMap.findIndex((line) => line.includes("S"));
+  const startIndex = tyleMap[startLine]!.findIndex((row) => row === "S");
+  for (let i = 0; i < possibleConnections.length; i++) {
+    const to = possibleConnections[i]!;
+    console.log("Possible connections", to);
+    // const from = getConnectionOpposite(to);
+    const { newPosition, from } = getNewPositionForConnection(to, {
+      line: startLine,
+      index: startIndex,
+    });
+    const steps = walkThroughMap(tyleMap, newPosition, 0, from);
+
+    if (steps > 0) {
+      return Math.ceil(steps / 2);
+    }
+  }
+
+  throw new Error("The start has no connection");
+};
+
+export const getTyleFromMap = (
+  tyleMap: TyleMap,
+  { line, index }: PositionInMap
+) => {
+  const tyle = tyleMap[line]![index];
+
+  if (tyle === undefined) {
+    throw new Error(`Line ${line} index ${index} is out of bounds`);
+  }
+  return tyle;
+};
+
+export const getNewPositionForConnection = (
+  to: Connection,
+  { line, index }: PositionInMap
+): { newPosition: PositionInMap; from: Connection } => {
+  switch (to) {
+    case "east":
+      return {
+        newPosition: { line, index: index + 1 },
+        from: getConnectionOpposite(to),
+      };
+    case "west":
+      return {
+        newPosition: { line, index: index - 1 },
+        from: getConnectionOpposite(to),
+      };
+    case "north":
+      return {
+        newPosition: { line: line - 1, index },
+        from: getConnectionOpposite(to),
+      };
+    case "south":
+      return {
+        newPosition: { line: line + 1, index },
+        from: getConnectionOpposite(to),
+      };
+  }
+};
+
+export const getConnectionOpposite = (connection: Connection): Connection => {
+  switch (connection) {
+    case "east":
+      return "west";
+    case "west":
+      return "east";
+    case "north":
+      return "south";
+    case "south":
+      return "north";
+  }
+};
+
+export const walkThroughMap = (
+  tyleMap: TyleMap,
+  position: PositionInMap,
+  steps: number,
+  connectedFrom?: Connection
+): number => {
+  const symbol = getTyleFromMap(tyleMap, position);
+  const connections = tyleConnections[symbol];
+
+  if (symbol === ".") {
+    console.log(
+      `The pipe reached a dead end at line:${position.line}, index:${position.index} connected from ${connectedFrom}`
+    );
+    return 0;
+  }
+
+  if (symbol === "S") {
+    return steps;
+  }
+
+  if (connections && connectedFrom) {
+    if (!connections.includes(connectedFrom)) {
+      console.log(
+        `The pipe ${symbol} cannot be connected from ${connectedFrom}`
+      );
+      return 0;
+    }
+    const to = connections.find((connection) => connection !== connectedFrom)!;
+    const { newPosition, from } = getNewPositionForConnection(to, position);
+    return walkThroughMap(tyleMap, newPosition, steps + 1, from);
+  }
+
+  console.error({ position, steps, connectedFrom });
+
+  throw new Error("This should not happen");
+};
